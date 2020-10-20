4a72ee8aba92b1070f1563ed0a744e2df57fc171
sehnoor-jamshaid <sehnoor.jam@gmail.com>
2020-10-21 01:15:25 +0500

    Updates

---
+++ precache-manifest.aa485a85506c349b3bfcabe17b1d1e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef2a4a8066b11e94e5af6308add4898",
+    "url": "/slotmachine/index.html"
+  },
+  {
+    "revision": "1f8c9abee9c5ec184eab",
+    "url": "/slotmachine/static/css/main.7dc54ae1.chunk.css"
+  },
+  {
+    "revision": "5040ee78fca3993d346b",
+    "url": "/slotmachine/static/js/2.bc922136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slotmachine/static/js/2.bc922136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8c9abee9c5ec184eab",
+    "url": "/slotmachine/static/js/main.c4679ace.chunk.js"
+  },
+  {
+    "revision": "36d7b91ef1efcdcc3997",
+    "url": "/slotmachine/static/js/runtime-main.3476b10d.js"
+  }
+]);
